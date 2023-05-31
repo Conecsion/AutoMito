e3ab28706b87c9b55a2c923d8dd00e13265f9465
shaodi <yangshaodi21@mails.ucas.ac.cn>
2023-05-31 08:13:43 +0800

    Testing

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-from crop import crop, crop_one
+from crop import crop
 from yolo_mito_detect import yolo_mito_detect
 from downsample import downsample
 from format_convert import format_convert
 import os
 
 from torch.utils.data import DataLoader
+from torch.utils.data import IterableDataset
+import torchvision.transforms as transforms
 import torch
 
+import matplotlib.pyplot as plt
+import itertools
 
-class GeneratorDataset(torch.utils.data.IterableDataset):
+size = 512
+
+
+class GeneratorDataset(IterableDataset):
 
     def __init__(self, generator):
         self.generator = generator
 
     def __iter__(self):
-        return iter(self.generator)
+        return self.generator
 
 
 if __name__ == "__main__":
-    crop(512)
-    results = yolo_mito_detect('0', 512, "model/yolo_mito.pt")
-    #  dataset = GeneratorDataset(results)
-    #  dataloader = DataLoader(dataset, batch_size=32)
-    #  for batch_idx, result_batch in enumerate(dataloader):
-    #  print(batch_idx)
-    #  print(result_batch)
-    for result in results:
-        print(result.boxes.xyxy)
+    crop(size)
+    results = yolo_mito_detect('0', size, "model/yolo_mito.pt")
+    transform = transforms.ToTensor()
+    # Plot image
+    #  tensor = tensor.permute(1,2,0)
+    #  plt.imshow(tensor)
+    #  plt.show()
+
+my_dict = [
+    dict(
+        zip(("image", "boxes", "original_size"),
+            (transform(result.orig_img), result.boxes.xyxy,
+             transform(result.orig_img).shape[:3]))) for result in results
+]
+print(my_dict)
