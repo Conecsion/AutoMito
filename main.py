18ae9fc96817583bb2dd2563aba988c125f66925
shaodi <yangshaodi21@mails.ucas.ac.cn>
2023-05-31 15:05:53 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -12,7 +12,18 @@ import torch
 import matplotlib.pyplot as plt
 import itertools
 
+import sys
+from segment_anything import sam_model_registry, SamPredictor
+
+sam_checkpoint = "model/sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+
 size = 512
+device = "cuda:0"
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+predictor = SamPredictor(sam)
 
 
 class GeneratorDataset(IterableDataset):
@@ -33,10 +44,11 @@ if __name__ == "__main__":
     #  plt.imshow(tensor)
     #  plt.show()
 
-my_dict = [
-    dict(
-        zip(("image", "boxes", "original_size"),
-            (transform(result.orig_img), result.boxes.xyxy,
-             transform(result.orig_img).shape[:3]))) for result in results
-]
-print(my_dict)
+    batched_input = [
+        dict(
+            zip(("image", "boxes", "original_size"),
+                (transform(result.orig_img).to(device), result.boxes.xyxy,
+                 transform(result.orig_img).shape[1:3]))) for result in results
+    ]
+
+    batched_output = sam(batched_input, multimask_output=False)
--- test.py
+++ test.py
@@ -2,9 +2,27 @@ import os
 import torch
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
+import torchvision.transforms as transform
+from torch.utils.data import DataLoader, IterableDataset
+import cv2
+import numpy as np
+from PIL import Image
+from segment_anything.utils.transforms import ResizeLongestSide
 
-dist.init_process_group(backend="nccl", world_size=N, init_method="...")
-global_rank = dist.get_rank()
-local_rank = global_rank % torch.cuda.device_count()
-print(global_rank)
-print(torch.cuda.device_count())
+
+def prepare_image(image):
+    print(image.shape)
+    image = torch.as_tensor(image)
+    print(image.shape)
+    return image.permute(2, 0, 1).contiguous()
+
+
+img = Image.open("output/cropped_images_png/Embryo1004_0_17.png")
+arr1 = np.array(img)
+print(arr1.shape)
+
+arr2 = cv2.imread("output/cropped_images_png/Embryo1004_0_17.png")
+print(arr2.shape)
+
+arr3 = prepare_image(arr1)
+print(arr3.shape)
