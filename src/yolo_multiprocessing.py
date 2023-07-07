8cad5fd90fd9a31ee6af70f142a35756887da14a
shaodi <yangshaodi21@mails.ucas.ac.cn>
2023-07-08 03:13:26 +0800

    230708

--- src/yolo_multiprocessing.py
+++ src/yolo_multiprocessing.py
@@ -4,8 +4,13 @@ import torch
 from ultralytics import YOLO
 
 # model = YOLO('/data/shaodi/DeepAi/yolo/DeepAI_Mito_Det/230706/weights/best.pt')
-model = torch.load(
-    '/data/shaodi/DeepAi/yolo/DeepAI_Mito_Det/230706/weights/best.pt')
-# model.export(format="-")
+# model = torch.load(
+#     '/data/shaodi/DeepAi/yolo/DeepAI_Mito_Det/230706/weights/best.pt')
 
-print(model.state_dict())
+# model = torch.hub.load()
+model = torch.hub.load(
+    '/data/shaodi/DeepAi/yolo/DeepAI_Mito_Det/230706/weights/best.pt',
+    'yolov8x',
+    source='local',
+    pretrained=True,
+)
